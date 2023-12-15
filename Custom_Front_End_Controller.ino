863f21113f4dbaa05512698dd9497659b95b768e
A-Looseal <flyguyparis@gmail.com>
2023-12-15 11:46:18 -0500

    Initial Commit (Oled Working)

---
+++ Custom_Front_End_Controller.ino
@@ -0,0 +1,55 @@
+
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// On an arduino UNO:       A4(SDA), A5(SCL), On an arduino MEGA 2560: 20(SDA), 21(SCL), On an arduino LEONARDO:   2(SDA),  3(SCL),
+#define OLED_RESET 2        // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup()
+{
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+  {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ; // Don't proceed, loop forever
+  }
+  Serial.println(F("SSD1306 allocation succedded"));
+
+  // Clear the buffer
+  display.clearDisplay();
+  delay(500);
+}
+
+void loop()
+{
+
+  testdrawstyles();
+  delay(5000);
+}
+
+void testdrawstyles(void)
+{
+  display.clearDisplay();
+
+  display.setTextSize(1);              // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);             // Start at top-left corner
+  display.println(F("Hello, world!"));
+
+  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
+  display.println(3.141592);
+
+  display.display();
+}
