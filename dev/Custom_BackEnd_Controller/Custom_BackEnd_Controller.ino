c828d271c609cd8e976222932b5bb5cedec6ba88
A-Looseal <flyguyparis@gmail.com>
2024-01-03 17:54:38 -0500

    fully functional front/back end

--- dev/Custom_BackEnd_Controller/Custom_BackEnd_Controller.ino
+++ dev/Custom_BackEnd_Controller/Custom_BackEnd_Controller.ino
@@ -1,56 +1,67 @@
 
 #include <ArduinoJson.h>
 #include <SoftwareSerial.h>
-StaticJsonDocument<200> doc;
+
+#define CLIENT_DEVICE_RX 2
+#define CLIENT_DEVICE_RX 3
+// settings for controlling device state. on|off
+#define ON_COMMAND 0
+#define OFF_COMMAND 1
+
+// buffer to store incoming data from the client
 char incomingData[200];
 
+// list of devices connected to the system
+#define DEVICE_1 4
+#define DEVICE_2 5
+#define DEVICE_3 6
+#define DEVICE_4 7
+#define DEVICE_5 8
+#define DEVICE_6 9
+#define DEVICE_7 10
+#define DEVICE_8 11
+
+// data storage containers
 int systemID;
 int deviceID;
 int desiredState;
 
-SoftwareSerial Serial1(2, 3);
+// create software serial object for client device connection
+SoftwareSerial ClientDevice(CLIENT_DEVICE_RX, CLIENT_DEVICE_RX);
+// create json document object
+StaticJsonDocument<200> doc;
 
 void setup()
 {
+    // connect to host computer via UART
     Serial.begin(9600);
-    Serial.println(F("connected to Serial 0..."));
+    Serial.println(F("connected to host on hardware serial 0."));
 
-    Serial1.begin(9600);
+    // connect to client device via UART
+    ClientDevice.begin(9600);
+    ClientDevice.flush();
+    Serial.println(F("connected to client on software serial 1."));
 
+    // set mode of the connected devices
+    pinMode(DEVICE_1, OUTPUT);
+    digitalWrite(DEVICE_1, OFF_COMMAND);
+    pinMode(DEVICE_2, OUTPUT);
+    digitalWrite(DEVICE_2, OFF_COMMAND);
+    pinMode(DEVICE_3, OUTPUT);
+    digitalWrite(DEVICE_3, OFF_COMMAND);
+    pinMode(DEVICE_4, OUTPUT);
+    digitalWrite(DEVICE_4, OFF_COMMAND);
+    // set mode of the led
     pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(8, OUTPUT);
-    pinMode(9, OUTPUT);
-
-    analogWrite(9, 0);
-
-    analogWrite(10, 255);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
-    analogWrite(10, 0);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
-
-    analogWrite(10, 255);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
-    analogWrite(10, 0);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
-
-    analogWrite(10, 255);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
-    analogWrite(10, 0);
-    digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop()
 {
     // get data
-    if (Serial1.available() > 0)
+    if (ClientDevice.available() > 0)
     {
         // store the incoming data
-        Serial1.readBytesUntil('\n', incomingData, 200);
+        ClientDevice.readBytesUntil('\n', incomingData, 200);
 
         // Deserialize the JSON document
         DeserializationError error = deserializeJson(doc, incomingData);
@@ -86,29 +97,22 @@ void loop()
     // if this device was selected
     if (systemID == 1)
     {
-        // put a check here
-        Serial.println(F("processed system id "));
-
         // check which device
         switch (deviceID)
         {
-        case 1: // first device connected to the system
-                // check desired state
-
-            // check here
-
+        case 1: // device 1
+                //  check desired state
             switch (desiredState)
             {
+                // turn device on
             case 1:
-                digitalWrite(LED_BUILTIN, HIGH);
-                analogWrite(10, 255);
-                // put a check here
+                digitalWrite(DEVICE_1, ON_COMMAND);
+                Led_Confirm();
                 break;
-
+                // turn device off
             case 0:
-                digitalWrite(LED_BUILTIN, LOW);
-                analogWrite(10, 0);
-                // put a check here
+                digitalWrite(DEVICE_1, OFF_COMMAND);
+                Led_Confirm();
                 break;
 
             default:
@@ -116,48 +120,73 @@ void loop()
             }
             break;
 
-            // device start
-        case 2: // first device connected to the system
+        case 2: // device 2
                 // check desired state
             switch (desiredState)
             {
             case 1:
-                digitalWrite(LED_BUILTIN, HIGH);
+                digitalWrite(DEVICE_2, ON_COMMAND);
+                Led_Confirm();
                 break;
-            case 0:
-                digitalWrite(LED_BUILTIN, LOW);
-                break;
-                // device end
 
-                // device start
-            case 3: // first device connected to the system
-                    // check desired state
-                switch (desiredState)
-                {
-                case 1:
-                    digitalWrite(LED_BUILTIN, HIGH);
-                    analogWrite(10, 255);   //beep on
-                    delay(1000);
-                    analogWrite(10, 0);     //beep off
-                    delay(1000);
-                    analogWrite(10, 255);   //beep on
-                    delay(1000);
-                    analogWrite(10, 0);     //beep off
-                    delay(1000);
-                    analogWrite(10, 255);   //beep on
-                    break;
-                case 0:
-                    digitalWrite(LED_BUILTIN, LOW);
-                    break;
-                    // device end
-                default:
-                    break;
-                }
+            case 0:
+                digitalWrite(DEVICE_2, OFF_COMMAND);
+                Led_Confirm();
                 break;
+
             default:
                 break;
             }
-        }
 
-        Serial.flush();
+        case 3: // device 3
+                // check desired state
+            switch (desiredState)
+            {
+            case 1:
+                digitalWrite(DEVICE_3, ON_COMMAND);
+                Led_Confirm();
+                break;
+            case 0:
+                digitalWrite(DEVICE_3, OFF_COMMAND);
+                Led_Confirm();
+                break;
+                // device end
+            default:
+                break;
+            }
+
+        case 4: // device 4
+                // check desired state
+            switch (desiredState)
+            {
+            case 1:
+                digitalWrite(DEVICE_4, ON_COMMAND);
+                Led_Confirm();
+                break;
+            case 0:
+                digitalWrite(DEVICE_4, OFF_COMMAND);
+                Led_Confirm();
+                break;
+                // device end
+            default:
+                break;
+            }
+        default:
+            break;
+        }
+        // clear the serial buffer
+        ClientDevice.flush();
+        *incomingData = 0;
+        // clear data containers
+        systemID = -1;
+        deviceID = -1;
+        desiredState = -1;
     }
+}
+
+void Led_Confirm()
+{
+    digitalWrite(LED_BUILTIN, ON_COMMAND);
+    delay(250);
+    digitalWrite(LED_BUILTIN, OFF_COMMAND);
+}
